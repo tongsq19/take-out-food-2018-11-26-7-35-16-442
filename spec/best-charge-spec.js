02f0ea5015b9df7774a4b0025b3123d4b96d3f15
tongsq19 <ubinexy@gmail.com>
2019-03-01 13:25:48 +0800

    Add Test for Scenario2

--- spec/best-charge-spec.js
+++ spec/best-charge-spec.js
@@ -8,6 +8,17 @@ describe("take out food", function() {
     expect("").toEqual(actual);
   });
 
+  it('Scenario2 : 单份菜品', function() {
+    let selectedItem = ["ITEM0013 x 1"];
+    let actual = main.bestCharge(selectedItem);
+    let expected="============= 订餐明细 =============\n" +
+      "肉夹馍 x 1 = 6元\n" +
+      "-----------------------------------\n" +
+      "总计：6元\n" +
+      "===================================";
+
+    expect(expected).toEqual(actual);
+  });
 
 });
 
