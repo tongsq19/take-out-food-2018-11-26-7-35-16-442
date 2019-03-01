cdc4c41a746f4743da956de9abc9d17623d74305
tongsq19 <ubinexy@gmail.com>
2019-03-01 14:06:24 +0800

    Pass Test for Scenario2

--- src/best-charge.js
+++ src/best-charge.js
@@ -3,9 +3,25 @@ let loadAllItems = require('./items.js');
 let loadPromotions = require('./promotions.js');
 
 
+let getItemById = function (itemID) {
+  let itemArray= loadAllItems();
+  let index = itemArray.findIndex(obj => obj.id === itemID);
+  return itemArray[index];
+};
+
 function bestCharge(selectedItems) {
   if(selectedItems.toString() === "") return "";
 
+  let checkout = "============= 订餐明细 =============\n";
+  let itemIDQuantityPair = selectedItems[0].split(" x ");
+  let item = getItemById(itemIDQuantityPair[0]);
+
+  checkout += item.name + " x 1 = " + item.price +"元\n";
+  checkout += "-----------------------------------\n";
+  checkout += "总计：" + item.price +"元\n";
+  checkout += "===================================";
+
+  return checkout;
 }
 
 
