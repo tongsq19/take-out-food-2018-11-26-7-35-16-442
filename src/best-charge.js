179ec862fb2fd2a87345882557cc06add64b350f
tongsq19 <ubinexy@gmail.com>
2019-03-01 15:22:41 +0800

    Pass Test for Scenario 5

--- src/best-charge.js
+++ src/best-charge.js
@@ -9,6 +9,40 @@ let getItemById = function (itemID) {
   return itemArray[index];
 };
 
+let isOrderSatisfyPromotionType2 = function (selectedItems) {
+  let promotionList = loadPromotions();
+  let index = promotionList.findIndex(obj => obj.type === "指定菜品半价");
+  let promotionItems = promotionList[index].items;
+
+  return promotionItems.reduce(function(prevResult, item) {
+    let isContain = selectedItems.toString().includes(item);
+    return prevResult | isContain;
+  }, false);
+};
+
+let calcPromotionType2 = function(selectedItems) {
+  let promotionList = loadPromotions();
+  let index = promotionList.findIndex(obj => obj.type === "指定菜品半价");
+  let promotionItems = promotionList[index].items;
+  let promotionItemName = promotionItems.map(id => getItemById(id).name);
+
+  let order = selectedItems.map(item => item.split(" x "));
+
+  let save = promotionItems.reduce(function(prevSum, itemId) {
+    let index = order.findIndex(item => itemId === item[0]);
+    if(index !== -1) {
+      let quantity = order[index][1];
+      return prevSum + 0.5*getItemById(itemId).price * quantity;
+    } else {
+      return prevSum;
+    }
+  }, 0);
+
+  return {nameString: promotionItemName.join("，"), save: save};
+};
+
+
+
 function bestCharge(selectedItems) {
   if(selectedItems.toString() === "") return "";
 
@@ -27,6 +61,15 @@ function bestCharge(selectedItems) {
     checkout += item.name + " x "+ itemQuantity + " = " + itemSumPrice +"元\n";
   });
 
+
+  if(isOrderSatisfyPromotionType2(selectedItems)) {
+    let result = calcPromotionType2(selectedItems);
+    checkout += "-----------------------------------\n";
+    checkout += "使用优惠:\n";
+    checkout += "指定菜品半价(" + result.nameString + ")，省"+result.save+"元\n";
+
+    totalPrice -= result.save;
+  }
 
   checkout += "-----------------------------------\n";
   checkout += "总计：" + totalPrice +"元\n";
